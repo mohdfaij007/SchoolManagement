4ef6cc231970c099ac29d04d7842497450214944
Faij <mohdfaij17@gmail.com>
2026-03-22 15:53:06 +0530

    adding vercel URL in web.config

--- src/main/java/com/schoolmanagement/schoolbackend/config/WebConfig.java
+++ src/main/java/com/schoolmanagement/schoolbackend/config/WebConfig.java
@@ -11,7 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**") // Apply to all endpoints
-				.allowedOrigins("http://localhost:4200") // Allow Angular app
+				.allowedOrigins("http://localhost:4200",
+						"https://school-management-ui-psi.vercel.app/") // Allow Angular app
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow these HTTP methods
 				.allowedHeaders("*") // Allow all headers
 				.allowCredentials(true);
